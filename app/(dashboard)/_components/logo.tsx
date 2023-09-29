42876349c1c08413a627b73ac8617679406d62f4
Cândido Júnior <candido101209@gmail.com>
2023-09-28 23:35:44 -0300

    added sidebar

---
+++ app/(dashboard)/_components/logo.tsx
@@ -0,0 +1,8 @@
+import Image from 'next/image'
+
+export const Logo = () => {
+    return (
+        <Image src="/logo.svg" alt="Logo"  width={100} height={100} />
+    )
+}
+    
---
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Layout, Compass } from 'lucide-react'
+import { SidebarItem } from "./sidebar-item";
+
+
+const gestRoutes = [
+    {
+        icon: Layout,
+        label: "Dashboard",
+        href: "/",
+    },
+    {
+        icon: Compass,
+        label: "Browse",
+        href: "/search",
+    }
+]
+
+export const SidebarRoutes = () => {
+    const routes = gestRoutes;
+
+    return (
+        <div className="flex flex-col w-full">
+            {routes.map((route) => (
+                <SidebarItem
+                key={route.href}
+                icon={route.icon}
+                label={route.label}
+                href={route.href}
+                />)
+            )}
+        </div>
+    )
+}

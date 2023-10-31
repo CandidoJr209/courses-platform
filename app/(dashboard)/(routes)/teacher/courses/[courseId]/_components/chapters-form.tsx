d136ff319dbd0ea471e00f76ed0bf268e96f9763
Cândido Júnior <candido101209@gmail.com>
2023-10-30 22:45:11 -0300

    chapter reorder

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -15,12 +15,13 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
-import { Pencil, PlusCircle } from "lucide-react";
+import { Loader2, Pencil, PlusCircle } from "lucide-react";
 import { toast } from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { Textarea } from "@/components/ui/textarea";
 import { Chapter, Course } from "@prisma/client";
+import { ChaptersList } from "./chapters-list";
 
 const formSchema = z.object({
     title: z.string().min(1),
@@ -62,8 +63,29 @@ export const ChaptersForm = (
         }
     }
 
+    const onReorder = async (updateData: { id: string, position: number }[]) => {
+        try {
+            setIsUpdating(true)
+
+            await axios.put(`/api/courses/${courseId}/chapters/reorder`, {
+                list: updateData
+            })
+            toast.success("Chapter reordered")
+            router.refresh()
+        } catch {
+            toast.error("Something went wrong")
+        } finally {
+            setIsUpdating(false)
+        }
+    }
+
     return (
-        <div className="mt-6 border bg-slate-100 rounded-md p-4">
+        <div className="relative mt-6 border bg-slate-100 rounded-md p-4">
+            {isUpdating && (
+                <div className="absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-m flex items-center justify-center">
+                    <Loader2 className="animate-spin h-6 w-6 text-sky-700"/>
+                </div>
+            )}
             <div className="font-medium flex items-center justify-between">
                 Course chapters
                 <Button onClick={toggleCreating} variant="ghost">
@@ -116,7 +138,11 @@ export const ChaptersForm = (
                     !initialData.chapters.length && "text-slate-500 italic"
                 )}>
                     {!initialData.chapters.length && "No chapters"}
-                    {/* TODO: Add a list of chapters */}
+                    <ChaptersList 
+                        onEdit={() => {}}
+                        onReorder={onReorder}
+                        items={initialData.chapters || []}
+                    />
                 </div>
             )}
             {!isCreating && (

d3518e596d8131b0ba4a2d85592f9704ef20205d
Cândido Júnior <candido101209@gmail.com>
2023-11-04 21:13:17 -0300

    chapter publish action

---
+++ components/modals/confirm-model.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { AlertDialogCancel } from "@radix-ui/react-alert-dialog"
+
+interface ConfirmModelProps {
+    children: React.ReactNode
+    onConfirm: () => void
+}
+
+export const ConfirmModal = ({
+    children,
+    onConfirm    
+    }: ConfirmModelProps) => {
+    return (
+        <AlertDialog>
+            <AlertDialogTrigger asChild>
+                {children}
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        This action cannot be undone
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction onClick={onConfirm}>
+                        Continue
+                    </AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+        )
+}
